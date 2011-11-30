e973fa421410c016d47ae54036f79735e175e1b6
WoLpH <Rick@Fawo.nl>
2011-11-30 05:06:06 -0800

    fixed the comment redirect somewhat

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tcc',
-    version='0.9.10',
+    version='0.9.11',
     description='Simple but effective comments app',
     #long_description=open('readme').read(),
     keywords='',
