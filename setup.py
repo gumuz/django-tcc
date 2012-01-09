f8bbd6422fa7b7c70316bfc2112e5c7c6c3e2a2f
Mike Ryan <mike@fadedink.co.uk>
2012-01-09 16:03:43 +0100

    bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tcc',
-    version='0.9.24',
+    version='0.9.25',
     description='Simple but effective comments app',
     #long_description=open('readme').read(),
     keywords='',
